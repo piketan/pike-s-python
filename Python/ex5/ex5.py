13f03a547640e23d2f74e796e252b26f462605b6
piketan <piketan@protonmail.com>
2020-05-18 16:34:38 +0800

    python

--- Python/ex5/ex5.py
+++ Python/ex5/ex5.py
@@ -1,7 +1,24 @@
-my_name = 'Zed A. Shaw'
+name = 'Zed A. Shaw'
 #变量的值如果加了单引号或者双引号的话就会变成字符串
-my_age = 35
-my_height = 74
-my_weight = 180
-my_eyes = 'Blue'
-my_teeth = 'white'
+age = 35
+height = 74
+weight = 180
+eyes = 'black'
+teeth = 'white'
+hair = 'black'
+one_cm = 2.54
+one_pound = 0.45359237
+
+
+print(f"让我们来谈谈{name}")
+print(f"这是我的身高{height}")
+print(f"这是我的体重{weight}")
+print(f"我的眼睛是{eyes}")
+print(f"我的牙齿是{teeth}")
+print(f"我的头发是{hair}")
+total = age+height+weight
+print(f"现在我要将我的年龄{age}我的身高{height}我的体重{weight}加在一起得到{total}")
+cm = height * one_cm
+si = weight * one_pound
+
+print(f"将英寸转换为厘米得出结果{cm},将磅转换为千克得出结果",round(si))
---
+++ Python/ex6/ex6.py
@@ -0,0 +1,18 @@
+types_of_people = 10
+x = f"这里有{types_of_people}个人"
+
+binary= "二进制"
+do_not = "不知道"
+y = f"知道{binary}的人和{do_not}的人"
+
+print(x)
+print(y)
+print(f"我说:{x}")
+print(f"我也说:'{y}'")
+hilaerious = 3>5
+joke_evaluation = "那个笑话不是很有趣吗?{}!"
+print(joke_evaluation.format(hilaerious))
+
+w = "这是弦的左边"
+e = "和右边"
+print(w+e)
