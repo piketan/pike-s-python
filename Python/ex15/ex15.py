e5ea52d5992796c9ab30d99b405345569a7248c2
piketan <piketan@protonmail.com>
2020-05-19 18:01:45 +0800

    Python

---
+++ Python/ex15/ex15.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script,filename = argv
+
+txt = open(filename)
+
+print(f"这是你的文件{filename}")
+print(txt.read())
+print("再一次输入文件名")
+file_again = input(">")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
